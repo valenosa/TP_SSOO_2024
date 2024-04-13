80d3c1b3817a953f606897995654501de78e5ad0
N3chh <imoreno@frba.utn.edu.ar>
2024-04-13 22:13:05 +0000

    primer aproach

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,5 +1,42 @@
 package main
 
 func main() {
+	/*
+		read(GET, body) => que hay que hacer
+		res
+		switch res
+		iniciar_proceso()
+	*/
+}
+
+func inciar_proceso() {
+	//implementar
+}
+
+func finalizar_proceso() {
+	//implementar
+}
+
+func estado_proceso() {
+	//implementar
+}
+
+func iniciar_planificacion() {
+	//implementar
+}
+
+func detener_planificacion() {
+	//implementar
+}
+
+func listar_proceso() {
+	//implementar
+}
+
+func dispatch() {
+
+}
+
+func interrupt() {
 
 }
