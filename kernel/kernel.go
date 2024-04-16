90e170428519c76a79776bd4e4f4bb2352d44d31
osavalentin <osavalentin.p@gmail.com>
2024-04-16 00:17:50 +0000

    Esqueleto iniciar_proceso() Se desarrolló la función iniciar_proceso() y las estructuras correspondientes a su body y respuesta recibida. Este commit es para probar el funcionamiento del código; luego se comentará cada parte del mismo.

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	/*
 		read(GET, body) => que hay que hacer
@@ -9,8 +16,55 @@ func main() {
 	*/
 }
 
+type BodyIniciarProceso struct {
+	Path string `json:"path"`
+}
+
+type ResponseIniciarProceso struct {
+	Pid string `json:"pid"`
+}
+
 func inciar_proceso() {
-	//implementar
+
+	ip := "localhost"
+	puerto := 8080
+
+	// Inicializar valor a enviar
+	body, err := json.Marshal(BodyIniciarProceso{
+		Path: "",
+	})
+
+	if err != nil {
+		fmt.Printf("error codificando body: %s", err.Error())
+	}
+
+	cliente := &http.Client{}
+
+	url := fmt.Sprintf("http://%s:%d/paquetes", ip, puerto)
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Printf("error creado request a ip:%s puerto:%d", ip, puerto)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	respuesta, err := cliente.Do(req)
+	if err != nil {
+		fmt.Printf("error enviando request a ip:%s puerto:%d", ip, puerto)
+	}
+
+	// Verificar el código de estado de la respuesta
+	if respuesta.StatusCode != http.StatusOK {
+		fmt.Printf("Status Error: %d", respuesta.StatusCode)
+	}
+
+	var response ResponseIniciarProceso
+	err = json.NewDecoder(respuesta.Body).Decode(&response)
+	if err != nil {
+		fmt.Printf("Error decodificando")
+	}
+
+	fmt.Println(response.Pid)
 }
 
 func finalizar_proceso() {
